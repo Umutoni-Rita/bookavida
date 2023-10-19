0715829de8b72c5d0117fdde61b16b4ce6c79730
Umutoni-Rita <umurita37@gmail.com>
2023-10-19 22:00:27 +0200

    add: summary

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import SearchBook from './components/search';
+// import SearchBook from './components/search';
+import BookSummary from './components/bookSummary';
 
 function App() {
   return (
     <div className="App">
-      <SearchBook />
+      <BookSummary />
     </div>
   );
 }
--- src/components/bookSummary.jsx
+++ src/components/bookSummary.jsx
@@ -1,2 +1,22 @@
-import React from 'react';
+import React from "react";
+import coverUnavailable from "../assets/cover-unavailable.jpeg";
 
+const BookSummary = ({
+  id,
+  coverSrc,
+  title,
+  author,
+  releaseYear,
+  bookLink,
+}) => {
+  return (
+    <div className=" bg-gray-950 flex bg-opacity-20">
+      <div className="md:w-2/5 w-[95%] h-[95%] flex flex-col justify-center items-center mx-auto my-6 bg-white rounded-lg">
+        <img src={coverSrc || coverUnavailable} alt="cover" />
+        <h1>{title || "title"}</h1>
+      </div>
+    </div> 
+  );
+};
+
+export default BookSummary;
